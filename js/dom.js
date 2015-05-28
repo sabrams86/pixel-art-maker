70e60352da992ad541adc1f235d6e3e38610202d
Steven Abrams <steven.abrams86@gmail.com>
2015-05-28 16:44:52 -0600

    added color picker

--- js/dom.js
+++ js/dom.js
@@ -1,12 +1,12 @@
 //assign main game area to a variable
 var container = document.getElementsByClassName('container')[0];
 //set up all of the divs
-var setArea = function(size){
-  for (var i = 0; i<size; i++){
+var setArea = function(width, height){
+  for (var i = 0; i<height; i++){
     var row = document.createElement('div');
     row.className = "row";
     container.appendChild(row);
-    for (var j = 0; j<size; j++){
+    for (var j = 0; j<width; j++){
       var cell = document.createElement('div');
       cell.className = "cell";
       row.appendChild(cell);
@@ -14,24 +14,24 @@ var setArea = function(size){
   }
 }
 
-setArea(10);
+setArea(10,10);
 
 //get all of the clickable divs
 var pixel = document.getElementsByClassName('cell');
 //get all of the colors
 var brush = document.getElementsByClassName('color');
 
+
+
 var brushColor;
 
-for (var i = 0; i < brush.length; i++){
-  brush[i].addEventListener('click', function(){
-    brushColor = getComputedStyle(this).getPropertyValue("background-color");
-    console.log(brushColor);
-    for (var i = 0; i < pixel.length; i++){
-      pixel[i].addEventListener('click', function(){
-        console.log(brushColor);
-        this.style.backgroundColor = brushColor;
-      });
-    }
+for (var i = 0; i < pixel.length; i++){
+  pixel[i].addEventListener('click', function(){
+    brushColor = document.getElementById('color-choice').value;
+    this.style.backgroundColor = brushColor;
+    this.style.border = "1px solid "+brushColor;
+    // this.addEventListener('dragenter', function(){
+    //   this.style.backgroundColor = brushColor;
+    // });
   });
 }
