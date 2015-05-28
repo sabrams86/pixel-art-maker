3c6e5ad66675a973ff568bea034d0cdea0f874d1
Steven Abrams <steven.abrams86@gmail.com>
2015-05-28 16:56:32 -0600

    added draggable

--- js/dom.js
+++ js/dom.js
@@ -15,18 +15,31 @@ var setArea = function(width, height){
 }
 
 setArea(10,10);
-
 //get all of the clickable divs
 var pixel = document.getElementsByClassName('cell');
 //get all of the colors
 var brush = document.getElementsByClassName('color');
 
-
-
 var brushColor;
 
 for (var i = 0; i < pixel.length; i++){
   pixel[i].addEventListener('click', function(){
+    brushColor = document.getElementById('color-choice').value;
+    this.style.backgroundColor = brushColor;
+    this.style.border = "1px solid "+brushColor;
+    // this.addEventListener('dragenter', function(){
+    //   this.style.backgroundColor = brushColor;
+    // });
+  });
+  pixel[i].addEventListener('dragstart', function(){
+    brushColor = document.getElementById('color-choice').value;
+    this.style.backgroundColor = brushColor;
+    this.style.border = "1px solid "+brushColor;
+    // this.addEventListener('dragenter', function(){
+    //   this.style.backgroundColor = brushColor;
+    // });
+  });
+  pixel[i].addEventListener('dragover', function(){
     brushColor = document.getElementById('color-choice').value;
     this.style.backgroundColor = brushColor;
     this.style.border = "1px solid "+brushColor;
